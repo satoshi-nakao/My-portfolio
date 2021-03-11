f67ec32fe2aa9d7d860f38a4b885d97803eb5741
Your Name <you@example.com>
2021-03-11 12:57:52 +0900

    newファイルを新規追加

---
+++ pages/new.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-row justify="center" align="center"> 
+    <v-col cols="12" sm="8" md="6">
+    <v-card>
+        <v-card-title>新機能</v-card-title>
+        <v-card-text>
+            <p>カーソルでなぞってみてください</p>
+            <vue-scratchable class="new">
+        　　　<h1>THANKS!</h1>
+    　　　　</vue-scratchable>
+        </v-card-text>
+    </v-card>
+    </v-col>
+    </v-row>
+</template>
+
+<style>
+.new{
+    margin: 0 auto;
+    min-height: 10vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    color: red;
+}
+</style>
